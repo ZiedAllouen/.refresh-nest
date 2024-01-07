d08d9ea0bda6d3534489676f813295178ee38f62
ziedAllouen <uchihazied@gmail.com>
2024-01-07 21:50:08 +0100

    last edit in back-end

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { ValidationError } from 'class-validator';
+import { ValidationException, ValidationFilter } from './util/filter.validation';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('/api')
+  app.useGlobalFilters(new ValidationFilter());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      skipMissingProperties: false,
+      exceptionFactory: (errors: ValidationError[]) => {
+        const errMsg = {};
+        errors.forEach((err) => {
+          errMsg[err.property] = [...Object.values(err.constraints)];
+        });
+        return new ValidationException(errMsg);
+      },
+    }),
+  );
   const port = process.env.PORT
   await app.listen(port);
 }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,5 @@
 import {
+  Body,
   Controller,
   Delete,
   Get,
@@ -8,38 +9,44 @@ import {
   Query,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { UserDto } from 'src/dto/users.dto';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersServices: UsersService) {}
 
   @Post()
-  Add() {
-    return 'Add';
+  Add(@Body() body: UserDto) {
+    return this.usersServices.Add(body);
   }
 
   @Get()
   FindAllUsers() {
-    return 'FindAllUsers';
+    return this.usersServices.FindAll();
   }
 
   @Get('/:id')
   FindOne(@Param('id') id: string) {
-    return id;
+    return this.usersServices.FindOne(id);
   }
 
   @Put('/:userId')
-  UpdateUser(@Param('userId') userId: string) {
-    return userId;
+  UpdateUser(@Param('userId') userId: string, @Body() body: UserDto) {
+    return this.usersServices.Update(userId, body);
   }
 
   @Delete('/:id')
   DeleteUser(@Param('id') id: string) {
-    return id;
+    return this.usersServices.Delete(id);
   }
 
   @Post('/search')
   Search(@Query('key') key: string) {
-    return key;
+    return this.usersServices.SearchUsers(key);
+  }
+
+  @Post('/faker')
+  Faker(){
+    return this.usersServices.Faker()
   }
 }
